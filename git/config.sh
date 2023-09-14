7fa18eda3281e71381fa7608d34d7cbce7ef42bd
Muthanna Alwahash <mkhuthir@gmail.com>
2023-09-14 22:01:38 +0400

    git

---
+++ git/config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Configuring ...."
+git config --global user.name "Muthanna Alwahash"
+git config --global user.email "mkhuthir@gmail.com"
+echo "Done."
+
+git --version
+git config --list
