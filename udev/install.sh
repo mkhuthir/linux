907c72e8c84fa0e7f7c47cbd56ccaab577096b73
Muthanna Alwahash <mkhuthir@gmail.com>
2023-09-21 21:15:28 +0400

    udev

--- udev/install.sh
+++ udev/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 sudo cp *.rules /etc/udev/rules.d/.
-sudo usermod -G plugdev -a $USER
+sudo usermod -a -G plugdev $USER
+sudo usermod -a -G dialout $USER
 sudo udevadm control --reload-rules
